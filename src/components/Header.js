09d9ab2a9028e23398749f37ece3f48444fc6d75
Lakhdher <bennejmanoureddine03@gmail.com>
2022-06-21 14:57:41 +0100

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -109,7 +109,6 @@ class Header extends Component {
         <Modal
           isOpen={this.state.isModalOpen}
           toggle={this.toggleModal} 
-     
         >
           <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
           <ModalBody>
